31fde8d909923f9807ce03c513e49e07586a188d
euuns <dmsu2@naver.com>
2025-03-25 21:56:26 +0900

    Feat: 애플리케이션에 설정을 위한  EnableJpaAuditing, EnableCaching, EnableSpringDataWebSupport 추가

--- src/main/java/com/example/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
+++ src/main/java/com/example/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
@@ -1,13 +1,21 @@
 package com.example.concurrencycontrolproject;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ConcurrencyControlProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConcurrencyControlProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyControlProjectApplication.class, args);
+	}
 
 }
