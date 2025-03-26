14122dd74ae15bd8bf4bcd4a7a3b14057b4ed77f
euuns <dmsu2@naver.com>
2025-03-26 10:44:46 +0900

    Feat: UserRole 추가

--- src/main/java/com/example/concurrencycontrolproject/domain/user/enums/UserRole.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/user/enums/UserRole.java
@@ -1,4 +1,29 @@
 package com.example.concurrencycontrolproject.domain.user.enums;
 
-public record UserRole() {
+import java.util.Arrays;
+
+import com.example.concurrencycontrolproject.domain.user.exception.InvalidUserRoleException;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+	ROLE_ADMIN(Authority.ADMIN),
+	ROLE_USER(Authority.USER);
+
+	private final String userRole;
+
+	public static UserRole of(String role) {
+		return Arrays.stream(UserRole.values())
+			.filter(r -> r.name().equalsIgnoreCase(role))
+			.findFirst()
+			.orElseThrow(() -> new InvalidUserRoleException());
+	}
+
+	public static class Authority {
+		public static final String ADMIN = "ROLE_ADMIN";
+		public static final String USER = "ROLE_USER";
+	}
 }
